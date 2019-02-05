195b26a88d85af8c576dfbf3dd927c22dbe1b182
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-02-05 09:58:17 +0530

    Remove minimist from argument parsing :tada:

--- cli.js
+++ cli.js
@@ -2,7 +2,6 @@
 
 const chalk = require('chalk')
 const program = require('commander')
-const minimist = require('minimist')
 const lib = require('.')
 const pkg = require('./package.json')
 
@@ -11,13 +10,21 @@ const createNextApp = lib.createNextApp
 
 let projectName
 
+const validateArgs = args => {
+  let hasSwitch = args.find(arg => arg.includes('-'))
+  if (hasSwitch) {
+    let index = args.indexOf(hasSwitch)
+    args.splice(index, 2)
+  }
+  return args
+}
 program
   .version(pkg.version)
   .arguments('<project-directory>')
   .usage(`${chalk.green('<project-directory>')} [options]`)
   .action(function(name) {
     projectName = name
-    if (minimist(process.argv.slice(2))._.length > 1) {
+    if (validateArgs(process.argv.slice(2)).length > 1) {
       messages.hasMultipleProjectNameArgs()
       process.exit(1)
     }
